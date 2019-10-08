0a0fee804836bca0c2d0abbcd53529a93dc06199
shrjamal <jamal.shr.18@gmail.com>
2019-10-08 18:08:19 +0100

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by ShrJamal on 10/8/19.
+//  Copyright © 2019 ShrJamal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let menu :[MenuSection] = Bundle.main.decode([MenuSection].self,from: "menu.json")
+    var body: some View {
+        NavigationView{
+            List{
+                ForEach( menu ){ section in
+                    Section(header: Text(section.name)) {
+                        ForEach(section.items){ item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }.navigationBarTitle("Menu")
+             .listStyle(GroupedListStyle())
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        ContentView()
+    }
+    
+    
+}
